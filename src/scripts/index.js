fd02da80d53960b8055fc2a129763b19a03a55db
Mike Lowe <mikelowedev@gmail.com>
2019-02-09 18:38:58 +0000

    Refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,263 +1,286 @@
+import _ from 'lodash';
+
 /*
  * Elements
  */
 
-const controls = document.querySelector('.controls');
-const depthControl = controls.querySelector('#depth');
-const angleControl = controls.querySelector('#angle');
-const rootLengthControl = controls.querySelector('#root-length');
-const shorterChildControl = controls.querySelector('#shorter-child');
-const rootWeightControl = controls.querySelector('#root-weight');
-const thinnerChildControl = controls.querySelector('#thinner-child');
-const rootColourControl = controls.querySelector('#root-colour');
-const multiColouredControl = controls.querySelector('#multi-coloured');
-const scaleControl = controls.querySelector('#scale');
-const resetContol = controls.querySelector('.reset');
-const closeControlsButton = controls.querySelector('.close');
+const controlsElement = document.querySelector('.js-controls');
+const depthElement = controlsElement.querySelector('.js-depth');
+const angleElement = controlsElement.querySelector('.js-angle');
+const rootLengthElement = controlsElement.querySelector('.js-root-length');
+const shorterChildElement = controlsElement.querySelector('.js-shorter-child');
+const rootWeightElement = controlsElement.querySelector('.js-root-weight');
+const thinnerChildElement = controlsElement.querySelector('.js-thinner-child');
+const rootColourElement = controlsElement.querySelector('.js-root-colour');
+const multiColouredElement = controlsElement.querySelector('.js-multi-coloured');
+const scaleElement = controlsElement.querySelector('.js-scale');
+const resetElement = controlsElement.querySelector('.js-reset');
+const closeContainerElement = controlsElement.querySelector('.js-close-container');
+const closeElement = controlsElement.querySelector('.js-close');
 
 /*
- * Properties
+ * Constants
  */
 
-const backgroundColour = [0, 0, 11.8];
-const minDepth = 1;
-const maxDepth = 15;
-const defaultDepth = 11;
-const minAngle = 0;
-const maxAngle = 360;
-const defaultAngle = 30;
-const minRootLength = 1;
-const maxRootLength = 200;
-const defaultRootLength = 170;
-const lengthMultiplier = 0.75; // used to determine child branch length
-const defaultShorterChild = true;
-const minRootWeight = 1;
-const maxRootWeight = 30;
-const defaultRootWeight = 1;
-const weightMultiplier = 0.9; // used to determine child branch weight
-const defaultThinnerChild = true;
-const defaultRootColour = '#ff81e6';
-const defaultMultiColoured = true;
-const hueMultiplier = 0.75; // used to determine child branch colour
-const minScale = 0.1;
-const maxScale = 2;
-const defaultScale = 1;
+const BACKGROUND_COLOUR = [0, 0, 11.8];
+const MIN_DEPTH = 1;
+const MAX_DEPTH = 15;
+const DEFAULT_DEPTH = 11;
+const MIN_ANGLE = 0;
+const MAX_ANGLE = 360;
+const DEFAULT_ANGLE = 30;
+const MIN_ROOT_LENGTH = 1;
+const MAX_ROOT_LENGTH = 200;
+const DEFAULT_ROOT_LENGTH = 170;
+const LENGTH_MULTIPLIER = 0.75; // used to determine child branch length
+const DEFAULT_SHORTER_CHILD = true;
+const MIN_ROOT_WEIGHT = 1;
+const MAX_ROOT_WEIGHT = 30;
+const DEFAULT_ROOT_WEIGHT = 1;
+const WEIGHT_MULTIPLIER = 0.9; // used to determine child branch weight
+const DEFAULT_THINNER_CHILD = true;
+const DEFAULT_ROOT_COLOUR = '#ff81e6';
+const DEFAULT_MULTI_COLOURED = true;
+const HUE_MULTIPLIER = 0.75; // used to determine child branch colour
+const MIN_SCALE = 0.1;
+const MAX_SCALE = 2;
+const DEFAULT_SCALE = 1;
+
+/*
+ * Variables.
+ */
+
 let controlsOpen = true;
 
 /*
  * Functions
  */
 
-function debounce(func, wait, immediate) {
-  let timeout;
-  return (...args) => {
-    const context = this;
-    const later = () => {
-      timeout = null;
-      if (!immediate) {
-        func.apply(context, args);
-      }
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) {
-      func.apply(context, args);
-    }
-  };
-}
+function hexToHsl(h) {
+  const hex = h.replace('#', '');
 
-function hexToHsl(hx) {
-  const hex = hx.replace('#', '');
-  const r = parseInt(hex.substring(0, 2), 16) / 255;
-  const g = parseInt(hex.substring(2, 4), 16) / 255;
-  const b = parseInt(hex.substring(4, 6), 16) / 255;
-  const max = Math.max(r, g, b);
-  const min = Math.min(r, g, b);
-  let h = 0;
-  let s = 0;
-  const l = (max + min) / 2;
+  const red = parseInt(hex.substring(0, 2), 16) / 255;
+  const green = parseInt(hex.substring(2, 4), 16) / 255;
+  const blue = parseInt(hex.substring(4, 6), 16) / 255;
+
+  const max = Math.max(red, green, blue);
+  const min = Math.min(red, green, blue);
+
+  let hue = 0;
+  let saturation = 0;
+  const lightness = (max + min) / 2;
+
   if (max !== min) {
-    const d = max - min;
-    s = (l > 0.5) ? d / (2 - max - min) : d / (max + min);
-    switch (max) {
-      case r:
-        h = (g - b) / d + (g < b ? 6 : 0);
-        break;
-      case g:
-        h = (b - r) / d + 2;
-        break;
-      case b:
-        h = (r - g) / d + 4;
-        break;
-      default:
-        break;
+    const delta = max - min;
+
+    saturation = (lightness > 0.5) ? delta / (2 - max - min) : delta / (max + min);
+
+    if (max === red) {
+      hue = (green - blue) / delta + (green < blue ? 6 : 0);
+    } else if (max === green) {
+      hue = (blue - red) / delta + 2;
+    } else if (max === blue) {
+      hue = (red - green) / delta + 4;
     }
-    h /= 6;
+
+    hue /= 6;
   }
-  return [h * 360, s * 100, l * 100];
+
+  return [hue * 360, saturation * 100, lightness * 100];
 }
 
-function branch(cd, md, angle, l, shorterChild, w,
-  thinnerChild, h, saturation, lightness, multiColoured) {
-  let currentDepth = cd;
-  let length = l;
-  let weight = w;
-  let hue = h;
+function branch(options) {
+  const {
+    maxDepth,
+    angle,
+    shorterChild,
+    thinnerChild,
+    saturation,
+    lightness,
+    multiColoured,
+  } = options;
+  let {
+    currentDepth,
+    length,
+    weight,
+    hue,
+  } = options;
+
   strokeWeight(weight);
   stroke(hue, saturation, lightness);
   line(0, 0, 0, -length);
   translate(0, -length);
-  if (currentDepth < md - 1) {
+
+  if (currentDepth < maxDepth - 1) {
     currentDepth += 1;
+
     if (shorterChild) {
-      length *= lengthMultiplier;
+      length *= LENGTH_MULTIPLIER;
     }
+
     if (thinnerChild) {
-      weight *= weightMultiplier;
+      weight *= WEIGHT_MULTIPLIER;
     }
+
     if (multiColoured) {
-      hue *= hueMultiplier;
+      hue *= HUE_MULTIPLIER;
     }
+
+    const childBranchOptions = {
+      currentDepth,
+      maxDepth,
+      angle,
+      length,
+      shorterChild,
+      weight,
+      thinnerChild,
+      hue,
+      saturation,
+      lightness,
+      multiColoured,
+    };
+
     push();
     rotate(angle);
-    branch(currentDepth, md, angle, length, shorterChild, weight,
-      thinnerChild, hue, saturation, lightness, multiColoured);
+    branch(childBranchOptions);
     pop();
+
     push();
     rotate(-angle);
-    branch(currentDepth, md, angle, length, shorterChild, weight,
-      thinnerChild, hue, saturation, lightness, multiColoured);
+    branch(childBranchOptions);
     pop();
   }
 }
 
-const drawTree = debounce(() => {
+const drawTree = _.debounce(() => {
   resizeCanvas(window.innerWidth, window.innerHeight);
-
-  background(backgroundColour);
+  background(BACKGROUND_COLOUR);
   translate(width / 2, height);
-  scale(parseFloat(scaleControl.value));
-  const currentDepth = 0;
-  const md = parseInt(depthControl.value, 10);
-  const angle = parseInt(angleControl.value, 10);
-  const length = parseInt(rootLengthControl.value, 10);
-  const shorterChild = shorterChildControl.checked;
-  const weight = parseInt(rootWeightControl.value, 10);
-  const thinnerChild = thinnerChildControl.checked;
-  const [hue, saturation, lightness] = hexToHsl(rootColourControl.value);
-  const multiColoured = multiColouredControl.checked;
-  branch(currentDepth, md, angle, length, shorterChild,
-    weight, thinnerChild, hue, saturation, lightness, multiColoured);
+  scale(parseFloat(scaleElement.value));
+
+  const [hue, saturation, lightness] = hexToHsl(rootColourElement.value);
+
+  branch({
+    currentDepth: 0,
+    maxDepth: parseInt(depthElement.value, 10),
+    angle: parseInt(angleElement.value, 10),
+    length: parseInt(rootLengthElement.value, 10),
+    shorterChild: shorterChildElement.checked,
+    weight: parseInt(rootWeightElement.value, 10),
+    thinnerChild: thinnerChildElement.checked,
+    hue,
+    saturation,
+    lightness,
+    multiColoured: multiColouredElement.checked,
+  });
 }, 100);
 
 function updateDepth() {
-  if (depthControl.value < minDepth) {
-    depthControl.value = minDepth;
-  } else if (depthControl.value > maxDepth) {
-    depthControl.value = maxDepth;
+  if (depthElement.value < MIN_DEPTH) {
+    depthElement.value = MIN_DEPTH;
+  } else if (depthElement.value > MAX_DEPTH) {
+    depthElement.value = MAX_DEPTH;
   } else {
-    depthControl.value = Math.round(depthControl.value);
+    depthElement.value = Math.round(depthElement.value);
   }
   drawTree();
 }
 
 function updateAngle() {
-  if (angleControl.value < minAngle) {
-    angleControl.value = minAngle;
-  } else if (angleControl.value > maxAngle) {
-    angleControl.value = maxAngle;
+  if (angleElement.value < MIN_ANGLE) {
+    angleElement.value = MIN_ANGLE;
+  } else if (angleElement.value > MAX_ANGLE) {
+    angleElement.value = MAX_ANGLE;
   } else {
-    angleControl.value = Math.round(angleControl.value);
+    angleElement.value = Math.round(angleElement.value);
   }
   drawTree();
 }
 
 function updateRootLength() {
-  if (rootLengthControl.value < minRootLength) {
-    rootLengthControl.value = minRootLength;
-  } else if (rootLengthControl.value > maxRootLength) {
-    rootLengthControl.value = maxRootLength;
+  if (rootLengthElement.value < MIN_ROOT_LENGTH) {
+    rootLengthElement.value = MIN_ROOT_LENGTH;
+  } else if (rootLengthElement.value > MAX_ROOT_LENGTH) {
+    rootLengthElement.value = MAX_ROOT_LENGTH;
   } else {
-    rootLengthControl.value = Math.round(rootLengthControl.value);
+    rootLengthElement.value = Math.round(rootLengthElement.value);
   }
   drawTree();
 }
 
 function updateRootWeight() {
-  if (rootWeightControl.value < minRootWeight) {
-    rootWeightControl.value = minRootWeight;
-  } else if (rootWeightControl.value > maxRootWeight) {
-    rootWeightControl.value = maxRootWeight;
+  if (rootWeightElement.value < MIN_ROOT_WEIGHT) {
+    rootWeightElement.value = MIN_ROOT_WEIGHT;
+  } else if (rootWeightElement.value > MAX_ROOT_WEIGHT) {
+    rootWeightElement.value = MAX_ROOT_WEIGHT;
   } else {
-    rootWeightControl.value = Math.round(rootWeightControl.value);
+    rootWeightElement.value = Math.round(rootWeightElement.value);
   }
   drawTree();
 }
 
 function updateRootColour() {
-  const validColour = /^#[0-9A-F]{6}$/i.test(rootColourControl.value);
+  const validColour = /^#[0-9A-F]{6}$/i.test(rootColourElement.value);
   if (!validColour) {
-    rootColourControl.value = defaultRootColour;
+    rootColourElement.value = DEFAULT_ROOT_COLOUR;
   }
   drawTree();
 }
 
 function updateScale() {
-  if (scaleControl.value < minScale) {
-    scaleControl.value = minScale;
-  } else if (scaleControl.value > maxScale) {
-    scaleControl.value = maxScale;
+  if (scaleElement.value < MIN_SCALE) {
+    scaleElement.value = MIN_SCALE;
+  } else if (scaleElement.value > MAX_SCALE) {
+    scaleElement.value = MAX_SCALE;
   }
   drawTree();
 }
 
 function reset() {
-  depthControl.value = defaultDepth;
-  angleControl.value = defaultAngle;
-  rootLengthControl.value = defaultRootLength;
-  shorterChildControl.checked = defaultShorterChild;
-  rootWeightControl.value = defaultRootWeight;
-  thinnerChildControl.checked = defaultThinnerChild;
-  rootColourControl.value = defaultRootColour;
-  multiColouredControl.checked = defaultMultiColoured;
-  scaleControl.value = defaultScale;
+  depthElement.value = DEFAULT_DEPTH;
+  angleElement.value = DEFAULT_ANGLE;
+  rootLengthElement.value = DEFAULT_ROOT_LENGTH;
+  shorterChildElement.checked = DEFAULT_SHORTER_CHILD;
+  rootWeightElement.value = DEFAULT_ROOT_WEIGHT;
+  thinnerChildElement.checked = DEFAULT_THINNER_CHILD;
+  rootColourElement.value = DEFAULT_ROOT_COLOUR;
+  multiColouredElement.checked = DEFAULT_MULTI_COLOURED;
+  scaleElement.value = DEFAULT_SCALE;
   drawTree();
 }
 
-function closeControls() {
+function close() {
   if (controlsOpen) {
-    const lastLi = controls.querySelector('ul li:last-child');
-    controls.style.transform = `translateY(-${lastLi.offsetTop}px)`;
-    closeControlsButton.textContent = 'Open Controls';
+    controlsElement.style.transform = `translateY(-${closeContainerElement.offsetTop}px)`;
+    closeElement.textContent = 'Open Controls';
   } else {
-    controls.style.transform = 'translateY(0px)';
-    closeControlsButton.textContent = 'Close Controls';
+    controlsElement.style.transform = 'translateY(0px)';
+    closeElement.textContent = 'Close Controls';
   }
   controlsOpen = !controlsOpen;
 }
 
-/*
- * Event listeners
- */
-
-depthControl.addEventListener('change', updateDepth);
-angleControl.addEventListener('change', updateAngle);
-rootLengthControl.addEventListener('change', updateRootLength);
-shorterChildControl.addEventListener('change', drawTree);
-rootWeightControl.addEventListener('change', updateRootWeight);
-thinnerChildControl.addEventListener('change', drawTree);
-rootColourControl.addEventListener('change', updateRootColour);
-multiColouredControl.addEventListener('change', drawTree);
-scaleControl.addEventListener('change', updateScale);
-resetContol.addEventListener('click', reset);
-closeControlsButton.addEventListener('click', closeControls);
-
-window.setup = () => {
+function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   colorMode(HSL);
   angleMode(DEGREES);
   drawTree();
-};
+}
 
+/*
+ * Initialise
+ */
+
+window.setup = setup;
 window.windowResized = drawTree;
+depthElement.addEventListener('change', updateDepth);
+angleElement.addEventListener('change', updateAngle);
+rootLengthElement.addEventListener('change', updateRootLength);
+shorterChildElement.addEventListener('change', drawTree);
+rootWeightElement.addEventListener('change', updateRootWeight);
+thinnerChildElement.addEventListener('change', drawTree);
+rootColourElement.addEventListener('change', updateRootColour);
+multiColouredElement.addEventListener('change', drawTree);
+scaleElement.addEventListener('change', updateScale);
+resetElement.addEventListener('click', reset);
+closeElement.addEventListener('click', close);
